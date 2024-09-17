0d962c6aea37a8352801918fa80782d56afbb6f8
Mdafsarx <mdafsar99009@gmail.com>
2024-09-17 19:08:23 +0600

    server site created

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,20 @@
 import Link from "next/link";
 
-export default function Login() {
+
+const getTime = async () => {
+  // const res = await fetch("http://localhost:3000/time", { cache: "no-store"})
+  const res = await fetch("http://localhost:3000/time", { next: { revalidate: 5 } })
+  const data = await res.json()
+  return data.currentTime
+}
+
+export default async function Login() {
+
+  const currentTime = await getTime()
+
   return (
     <div>
+      <p>Time: {currentTime}</p>
       <p>
         <button className="btn btn-success btn-outline">Success</button>
       </p>
